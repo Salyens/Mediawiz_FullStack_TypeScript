2919d05a586bc605f435dc021e99674fc0062d9a
Артем Зотов <artem.zotov@webpp.ru>
2025-03-18 15:49:44 +0300

    Add Dockerfile and update package dependencies; comment out image processing code

--- components/BlurImg/index.tsx
+++ components/BlurImg/index.tsx
@@ -1,4 +1,4 @@
-import { generateBlurDataURL } from "@utils/generateBlurDataURL";
+
 import classNames from "classnames";
 import Image from "next/image";
 
@@ -10,13 +10,13 @@ interface BlurImgProps {
 }
 
 const BlurImg = async ({ imgURL, name, classes, sizes }: BlurImgProps) => {
-  let base64 = "";
+  // let base64 = "";
 
-  try {
-    base64 = await generateBlurDataURL(imgURL);
-  } catch (error) {
-    return null;
-  }
+  // try {
+  //   base64 = await generateBlurDataURL(imgURL);
+  // } catch (error) {
+  //   return null;
+  // }
 
   return (
     <Image
@@ -28,8 +28,8 @@ const BlurImg = async ({ imgURL, name, classes, sizes }: BlurImgProps) => {
         "object-contain select-none pointer-events-none touch-none",
         classes
       )}
-      placeholder="blur"
-      blurDataURL={base64}
+      // placeholder="blur"
+      // blurDataURL={base64}
       priority={true}
     />
   );
--- utils/generateBlurDataURL.ts
+++ utils/generateBlurDataURL.ts
@@ -1,17 +1,17 @@
-import sharp from 'sharp';
+// import sharp from 'sharp';
 
-export const generateBlurDataURL = async (imgURL:string) => {
-  const response = await fetch(imgURL);
-  const buffer = await response.arrayBuffer();
+// export const generateBlurDataURL = async (imgURL:string) => {
+//   const response = await fetch(imgURL);
+//   const buffer = await response.arrayBuffer();
 
-  const imageBuffer = Buffer.from(buffer);
+//   const imageBuffer = Buffer.from(buffer);
 
-  const { data, info } = await sharp(imageBuffer)
-    .resize({ width: 50 })
-    .png()
-    .toBuffer({ resolveWithObject: true });
+//   const { data, info } = await sharp(imageBuffer)
+//     .resize({ width: 50 })
+//     .png()
+//     .toBuffer({ resolveWithObject: true });
 
-  const base64 = `data:image/png;base64,${data.toString('base64')}`;
+//   const base64 = `data:image/png;base64,${data.toString('base64')}`;
 
-  return base64;
-};
+//   return base64;
+// };
