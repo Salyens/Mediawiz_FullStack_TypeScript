48e38704c5ad39a6499c374803143222ddb04442
Артем Зотов <artem.zotov@webpp.ru>
2025-04-08 18:47:22 +0400

    Remove webpack configuration for image file handling from next.config.mjs, retaining only the content security policy settings for improved clarity.

--- next.config.mjs
+++ next.config.mjs
@@ -13,22 +13,7 @@ const nextConfig = {
       },
     ],
     dangerouslyAllowSVG: true,
-    contentSecurityPolicy:
-      "default-src 'self'; script-src 'none'; sandbox;",
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: [/\.(png|jpg|gif)$/, /_raw\.svg$/],
-      use: {
-        loader: "url-loader",
-        options: {
-          limit: 16 * 1024,
-          name: "[name]-[hash].[ext]",
-        },
-      },
-    });
-
-    return config;
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
