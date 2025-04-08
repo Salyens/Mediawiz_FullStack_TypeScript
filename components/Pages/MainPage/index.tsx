77ec380391620891f19cfebbd01640c48daf9572
Артем Зотов <artem.zotov@webpp.ru>
2025-04-08 13:46:58 +0400

    Implement dynamic imports for components in MainPage to improve performance and reduce initial load time.

--- components/Pages/MainPage/index.tsx
+++ components/Pages/MainPage/index.tsx
@@ -5,6 +5,40 @@ import FeedbackForm from "@components/FeedbackForm";
 import TogetherBlock from "./TogetherBlock";
 import Welcome from "@components/Welcome";
 import Quotes from "@components/Quotes";
+import dynamic from "next/dynamic";
+
+const DynamicWeOffer = dynamic(() => import("./WeOffer"), {
+  ssr: false,
+});
+
+const DynamicSocials = dynamic(() => import("./Socials"), {
+  ssr: false,
+});
+
+const DynamicQuotes = dynamic(
+  () => import("@components/Quotes"),
+  {
+    ssr: false,
+  }
+);
+
+const DynamicTogetherBlock = dynamic(
+  () => import("./TogetherBlock"),
+  {
+    ssr: false,
+  }
+);
+
+const DynamicFeedbackForm = dynamic(
+  () => import("@components/FeedbackForm"),
+  {
+    ssr: false,
+  }
+);
+
+const DynamicWelcome = dynamic(() => import("@components/Welcome"), {
+  ssr: false,
+});
 
 const MainPage = ({ data }: { data: LanguageContent }) => {
   const { main } = data;
@@ -15,13 +49,17 @@ const MainPage = ({ data }: { data: LanguageContent }) => {
 
   return (
     <div className="w-full h-full min-h-screen overflow-hidden relative">
-      <Welcome main={main} page="home" />
-      <WeOffer weOffer={weOffer} />
-      <Socials />
-      <Quotes quotesList={quotesList} />
-      <TogetherBlock imgURL={imgURL} goalsList={goalsList} />
-      <FeedbackForm />
+      <DynamicWelcome main={main} page="home" />
+      <DynamicWeOffer weOffer={weOffer} />
+      <DynamicSocials />
+      <DynamicQuotes quotesList={quotesList} />
+      <DynamicTogetherBlock
+        imgURL={imgURL}
+        goalsList={goalsList}
+      />
+      <DynamicFeedbackForm />
     </div>
   );
 };
+
 export default MainPage;
