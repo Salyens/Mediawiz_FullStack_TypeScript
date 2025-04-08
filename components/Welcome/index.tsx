70e95de53fdb797f1c8b625bd56dba22574825a9
Артем Зотов <artem.zotov@webpp.ru>
2025-04-08 14:34:06 +0400

    Remove BackgroundVideo component from Welcome and ensure consistent rendering of BgEllipse for improved layout stability.

--- components/Welcome/index.tsx
+++ components/Welcome/index.tsx
@@ -3,7 +3,6 @@ import { IWebMainSection } from "@interfaces/common";
 import { ISmmMainSection } from "@interfaces/smmPage";
 import WelcomeDesc from "./WelcomeDesc";
 import WelcomeImg from "./WelcomeImg";
-import BackgroundVideo from "@components/BackgroundVideo";
 import BgEllipse from "@components/BgEllipse";
 
 interface WebHeaderProps {
@@ -18,16 +17,12 @@ const Welcome: React.FC<WebHeaderProps> = ({
   const { imgURL } = main.img;
   return (
     <div className="relative lg:h-screen flex flex-col justify-center">
-      {page === "home" ? (
-        <BackgroundVideo />
-      ) : (
-        <BgEllipse
-          variant={1}
-          position="md:-left-1/4 md:-top-1/3"
-          width={1300}
-          height={1300}
-        />
-      )}
+      <BgEllipse
+        variant={1}
+        position="md:-left-1/4 md:-top-1/3"
+        width={1300}
+        height={1300}
+      />
 
       <div className="flex justify-between w-full items-center main_container mb-8 p-3 sm:p-4 md:p-5 xl:p-6 pt-16 sm:pt-24 md:pt-32 ">
         <WelcomeDesc main={main} page={page} />
