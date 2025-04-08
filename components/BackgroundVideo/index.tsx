5db6dc2f3f66f7a144c37b68d18429dd5ca2883f
Артем Зотов <artem.zotov@webpp.ru>
2025-04-08 18:20:47 +0400

    Update BackgroundVideo source and remove its usage from Welcome component to streamline layout and improve performance.

--- components/BackgroundVideo/index.tsx
+++ components/BackgroundVideo/index.tsx
@@ -7,7 +7,7 @@ const BackgroundVideo: React.FC = () => {
       playsInline
       className="absolute inset-0 w-full h-screen object-cover -z-10"
     >
-      <source src="/mainPage/welcome_bg.mp4" type="video/mp4" />
+      <source src="/mainPage/1.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
